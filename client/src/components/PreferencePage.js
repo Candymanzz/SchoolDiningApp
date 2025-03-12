52ca8f4eb8c499970e9fefb0d33c5e3c1e56a6fe
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-12 10:49:59 +0300

    Create PreferencePage.js

---
+++ client/src/components/PreferencePage.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchPreferences } from '../store/preferenceSlice';
+import PreferenceList from './PreferenceList';
+import CreatePreference from './CreatePreference';
+
+const PreferencePage = () => {
+    const dispatch = useDispatch();
+    const preferences = useSelector((state) => state.preferences.items);
+
+    useEffect(() => {
+        dispatch(fetchPreferences());
+    }, [dispatch]);
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h2>Предпочтения учащихся</h2>
+                    <PreferenceList preferences={preferences} />
+                </Col>
+                <Col>
+                    <h2>Добавить новое предпочтение</h2>
+                    <CreatePreference />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default PreferencePage;
