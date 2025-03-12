a2850ea7f6d484c87b42625c8196493f35702395
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-12 10:51:53 +0300

    update index router

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const studentRouter = require('./studentRouter')
 const attendanceRouter = require('./attendanceRouter')
 const nutritionRouter = require('./nutritionRouter')
 const participantRouter = require('./participantRouter')
+const preferenceRouter = require('./preferenceRouter')
 
 router.use('/employee', employeeRouter)
 router.use('/class', classRouter)
@@ -14,5 +15,6 @@ router.use('/student', studentRouter)
 router.use('/attendance', attendanceRouter)
 router.use('/nutrition', nutritionRouter)
 router.use('/participant', participantRouter)
+router.use('/preference', preferenceRouter)
 
 module.exports = router
