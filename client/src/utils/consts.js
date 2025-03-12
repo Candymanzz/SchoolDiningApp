f6dc2e48e1f5d02a8b746984d530377243861052
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-12 10:50:53 +0300

    add PREFERENCE_ROUTE

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,8 +1,9 @@
 export const EMPLOYEES_ROUTE = '/employees'
-export const ADMIN_ROUTE = '/admin' //
-export const LOGIN_ROUTE = '/login' //
-export const REGISTRATION_ROUTE = '/registration' //
+export const ADMIN_ROUTE = '/admin'
+export const LOGIN_ROUTE = '/login'
+export const REGISTRATION_ROUTE = '/registration'
 export const NUTRITIONS_ROUTE = '/nutrition'
 export const CLASSES_ROUTE = '/classes'
 export const ATTENDANCE_ROUTE = '/attendance'
-export const MAIN_ROUTE = '/' //
+export const PREFERENCE_ROUTE = '/preference'
+export const MAIN_ROUTE = '/' 
