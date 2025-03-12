a5a60fdffa0992b258c5a721d2fc6ddbe0bfa2d9
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-12 10:50:21 +0300

    Create PreferencesMenu.js

---
+++ client/src/pages/PreferencesMenu.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { setPreferences, setTotalCount, setPage } from '../store/preferencesSlice'
+import Pages from '../components/Pages'
+import PreferenceList from '../components/PreferenceList'
+import { fetchPreferences } from '../http/modelAPI'
+
+export default function PreferenceMenu() {
+    const { preferences, page, totalCount, limit } = useSelector((state) => state.preferences)
+    const dispatch = useDispatch()
+
+    const handlePreferences = (data) => dispatch(setPreferences(data))
+    const handleTotalCount = (count) => dispatch(setTotalCount(count))
+    const handlePage = (p) => dispatch(setPage(p))
+
+    useEffect(() => {
+        fetchPreferences(page, limit).then(data => {
+            handlePreferences(data.rows)
+            handleTotalCount(data.count)
+        })
+    }, [page])
+
+    return (
+        <Container>
+            <Row className="mt-2">
+                <Col className="mt-2">
+                    <PreferenceList preferences={preferences} />
+                    <Pages totalCount={totalCount} limit={limit} page={page} handlePage={handlePage} />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
