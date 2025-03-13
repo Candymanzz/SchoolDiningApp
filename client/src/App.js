e7849eff9888d4de46afa425d912d93659dc9c31
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-13 11:35:51 +0300

    mini fix

--- client/src/App.js
+++ client/src/App.js
@@ -8,38 +8,48 @@ import { useDispatch } from 'react-redux'
 import { Container, Spinner } from "react-bootstrap";
 
 function App() {
-  const [loading, SetLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   const dispatch = useDispatch();
 
   const handleEmployee = (ob) => {
-    dispatch(setEmployee(ob))
-  }
+    dispatch(setEmployee(ob));
+  };
+
   const handleAuth = (bool) => {
-    dispatch(setIsAuth(bool))
-  }
+    dispatch(setIsAuth(bool));
+  };
+
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      try {
-        check().then(data => {
-          handleEmployee(data)
-          handleAuth(true)
-        }).finally(() => SetLoading(false))
-      } catch {
-        handleEmployee({})
-        handleAuth(false)
-        localStorage.removeItem('token')
-        SetLoading(false)
+    const fetchData = async () => {
+      if (localStorage.getItem("token")) {
+        try {
+          const data = await check();
+          handleEmployee(data);
+          handleAuth(true);
+        } catch (error) {
+          handleEmployee({});
+          handleAuth(false);
+          localStorage.removeItem('token');
+        } finally {
+          setLoading(false);
+        }
+      } else {
+        setLoading(false);
       }
-    }
-    else {
-      SetLoading(false)
-    }
-  }, [])
+    };
+
+    fetchData();
+  }, []);
 
   if (loading) {
-    return <Container className="d-flex justify-content-center align-items-center" style={{ height: window.innerHeight - 54 }}><Spinner animation={"grow"} /></Container>
+    return (
+      <Container className="d-flex justify-content-center align-items-center" style={{ height: window.innerHeight - 54 }}>
+        <Spinner animation={"grow"} />
+      </Container>
+    );
   }
+
   return (
     <BrowserRouter>
       <NavBar />
