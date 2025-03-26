f370a00c97848743517e036a2ed6b4bc07340d26
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-03-26 23:47:29 +0300

    feat: add server index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
